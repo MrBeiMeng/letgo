4e6243437c6c148f96f80f57b75629f99410a561
baolong <1192384722@qq.com>
2023-01-06 12:01:41 +0800

    code H 指数 反向计数排序

--- code_lists/letgo_h_index.go
+++ code_lists/letgo_h_index.go
@@ -1,12 +1,12 @@
 package code_lists
 
-import "sort"
-
 /*H 指数 | https://leetcode.cn/problems/h-index*/
 
 func hIndex(citations []int) int {
 
-	sort.Sort(sort.Reverse(sort.IntSlice(citations)))
+	//sort.Sort(sort.Reverse(sort.IntSlice(citations)))
+
+	hIndexCountSort(citations)
 
 	h := 0
 	for i, citation := range citations {
@@ -15,6 +15,37 @@ func hIndex(citations []int) int {
 	}
 
 	return h
+}
+
+func hIndexCountSort(nums []int) {
+	if len(nums) == 1 {
+		return
+	}
+
+	rightBound := -1
+	leftBound := len(nums)
+
+	for _, num := range nums {
+		rightBound = hIndexMax(rightBound, num)
+		leftBound = hIndexMin(leftBound, num)
+	}
+
+	sortList := make([]int, rightBound-leftBound+1)
+
+	for _, num := range nums {
+		sortList[num-leftBound] += 1
+	}
+
+	nums = nums[:0]
+	// 正向排序
+
+	// 反向排序
+	for i := len(sortList) - 1; i >= 0; i-- {
+		times := sortList[i]
+		for j := 0; j < times; j++ {
+			nums = append(nums, i+leftBound)
+		}
+	}
 }
 
 func hIndexMin(a, b int) int {
